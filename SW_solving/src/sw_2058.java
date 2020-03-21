107b177ed1d88e9430f5bc194585e87ea3ca5493
seohpark <59459267+seohpark@users.noreply.github.com>
2020-03-21 15:43:50 +0900

    sw_2058
    자릿수 더하기

---
+++ SW_solving/src/sw_2058.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class sw_2058 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Scanner sc = new Scanner(System.in);
+	
+	int N = sc.nextInt();
+	int sum=0;
+	for(int i=0; i<4; i++) {
+		//int num = N/10;
+		int last = N%10;
+		N = N/10;
+		sum += last;
+		if(N < 10) {
+		sum += N;
+		break;
+		}
+	}	
+	System.out.println(sum);
+	}
+}
